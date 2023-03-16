53e5148f3c61ba90e8b7c97e3ec44c15f96a9618
kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-03-16 14:22:45 +0000

    ログファイルを無効化

--- logger.js
+++ logger.js
@@ -4,14 +4,14 @@ module.exports.logger = pino({
   level: 'trace',
   transport: {
     targets: [
-      {
+      /* {
         level: 'trace',
         target: 'pino/file',
         options: {
           destination: 'logs/out.log',
           mkdir: true
         }
-      },
+      }, */
       {
         level: 'trace',
         target: './console-log.mjs'
