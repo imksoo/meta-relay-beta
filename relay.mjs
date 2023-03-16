b5febb2e4f320081e9ef584363b640fc963562b1
kirino.minato@gmail.com <kirino.minato@gmail.com>
2023-03-16 14:23:27 +0000

    上流リレーとの接続タイミングを変更。

--- relay.mjs
+++ relay.mjs
@@ -4,14 +4,16 @@ import { WebSocket, WebSocketServer } from 'ws'
 import stringify from 'json-stable-stringify'
 
 const relay = new WebSocketServer({ port: 8080 })
-const upstream = new WebSocket('wss://relay.nostr.wirednet.jp')
 
 const MAX_HISTORY_SIZE = 1000
 const REMOVAL_RATIO = 0.2
 
 relay.on('connection', (downstream, request) => {
+  const upstream = new WebSocket('wss://relay.nostr.wirednet.jp')
+
   logger.info('downstream open')
   const remoteIp = request.socket.remoteAddress
+  const remortPort = request.socket.remotePort
   const fowardedIpList = request.headers['x-forwarded-for'].split(',')
 
   const messages = new Set()
@@ -19,11 +21,11 @@ relay.on('connection', (downstream, request) => {
   let suppressedMessages = 0
   let mutedMessages = 0
 
-  upstream.on('error', logger.error)
+  upstream.on('error', console.log)
   upstream.on('open', () => {
-    logger.info('upstream open')
+    // logger.info('upstream open')
   })
-  upstream.on('message', (data) => {
+  upstream.on('message', async (data) => {
     const msg = JSON.parse(data)
     // logger.info({ remoteIp, fowardedIpList, data: JSON.stringify(msg) }, 'upstream.on(message)')
 
@@ -31,32 +33,40 @@ relay.on('connection', (downstream, request) => {
     const eventJSON = stringify(msg)
     if (!messages.has(eventJSON)) {
       messages.add(eventJSON)
-      if (eventJSON.indexOf('avive.world') > 0 || eventJSON.indexOf('lnbc') > 0 || eventJSON.indexOf('web3') > 0 || eventJSON.indexOf('海外留学生') > 0 || eventJSON.indexOf('交流') > 0) {
-        logger.info('mute keyword detects in message.')
+
+      let muteKeywordMatched = false
+      const muteKeywords = ['avive', 'lnbc', 'web3', '海外留学生', '交流']
+      muteKeywords.forEach(keyword => {
+        if (eventJSON.indexOf(keyword) !== -1) {
+          muteKeywordMatched = true
+        }
+      })
+      if (muteKeywordMatched) {
+        // logger.info('mute keyword detects in message.')
         ++mutedMessages
       } else {
         downstream.send(eventJSON)
       }
     } else {
-      logger.info('duplicate message suppressed.')
+      // logger.info('duplicate message suppressed.')
       ++suppressedMessages
     }
 
     if (messages.size > MAX_HISTORY_SIZE) {
-      logger.trace('message history size = ' + messages.size)
+      // logger.trace('message history size = ' + messages.size)
       const numToRemove = Math.floor(messages.size * REMOVAL_RATIO)
       for (let i = 0; i < numToRemove; ++i) {
         messages.delete(messages.values().next().value)
       }
-      logger.info('message reduced. numToRemove=' + numToRemove)
-      logger.trace('message history size = ' + messages.size)
+      // logger.info('message reduced. numToRemove=' + numToRemove)
+      // logger.trace('message history size = ' + messages.size)
     }
 
-    logger.info({ suppressedMessages, mutedMessages, totalMessages })
+    logger.info({ remoteIp, remortPort, fowardedIpList, suppressedMessages, mutedMessages, totalMessages })
   })
 
-  downstream.on('error', logger.error)
-  downstream.on('message', (data) => {
+  downstream.on('error', console.log)
+  downstream.on('message', async (data) => {
     const msg = JSON.parse(data)
     logger.info({ remoteIp, fowardedIpList, data: JSON.stringify(msg) }, 'downstream.on(message)')
 
@@ -66,6 +76,8 @@ relay.on('connection', (downstream, request) => {
     } else {
       upstream.send(JSON.stringify(msg))
     } // */
-    upstream.send(JSON.stringify(msg))
+    if (upstream.readyState) {
+      upstream.send(JSON.stringify(msg))
+    }
   })
 })
